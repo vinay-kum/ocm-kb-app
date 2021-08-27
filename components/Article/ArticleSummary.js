aa06e061d49106cb928c1ef6733df0c04f7e83ad
Vinay Kumar <mailbox4vinay@gmail.com>
2021-08-27 11:01:52 +0530

    Fixed the error with Article Detail link

--- components/Article/ArticleSummary.js
+++ components/Article/ArticleSummary.js
@@ -2,11 +2,11 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 
-export default function ArticleSummary({data,key}) {
+export default function ArticleSummary({data,id}) {
   const router = useRouter()
     return (
         <div className="article-summary p-3">
-        <p onClick={()=>router.push(`/article/${key}`)}><h2 className="title">{data.title}</h2></p>
+        <p onClick={()=>router.push(`/article/${id}`)}><h2 className="title">{data.title}</h2></p>
         <div className="cat-summary">{data.summary}</div>
       </div>
     )
